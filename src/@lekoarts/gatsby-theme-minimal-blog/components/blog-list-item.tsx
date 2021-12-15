5e3e874d31563aa19eb0576233a8998e53ef213c
gdonner <gtdonner1@gmail.com>
2021-12-15 14:34:07 -0500

    remove - from blog-list-item

--- src/@lekoarts/gatsby-theme-minimal-blog/components/blog-list-item.tsx
+++ src/@lekoarts/gatsby-theme-minimal-blog/components/blog-list-item.tsx
@@ -29,7 +29,7 @@ const BlogListItem = ({ post, showTags = true }: BlogListItemProps) => (
       {/* <time>{post.date}</time> */}
       {post.tags && showTags && (
         <React.Fragment>
-          {` — `}
+          {/* {` — `} */}
           <ItemTags tags={post.tags} />
         </React.Fragment>
       )}
