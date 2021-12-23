06a941c3c84405c73ca8ac82ea036bd6fd84ffdb
gdonner <gtdonner1@gmail.com>
2021-12-23 17:53:13 -0500

    added gatsby-plugin-image

--- gatsby-config.js
+++ gatsby-config.js
@@ -71,6 +71,9 @@ module.exports = {
       },
     },
     `gatsby-plugin-sitemap`,
+    `gatsby-plugin-image`,
+    `gatsby-plugin-sharp`,
+    `gatsby-transformer-sharp`, // Needed for dynamic images
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
