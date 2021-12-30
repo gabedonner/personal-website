2e378af95bfbbad492fb3dbf6f80fac02dbc9c5e
gdonner <gtdonner1@gmail.com>
2021-12-30 13:07:39 -0500

    change to homepage query to display more posts

--- src/@lekoarts/gatsby-theme-minimal-blog-core/src/templates/homepage-query.tsx
+++ src/@lekoarts/gatsby-theme-minimal-blog-core/src/templates/homepage-query.tsx
@@ -3,6 +3,7 @@ import HomepageComponent from "../components/homepage"
 
 export default HomepageComponent
 
+//this might be where i can dictate the about of posts on homepage
 export const query = graphql`
   query ($formatString: String!) {
     allPost(sort: { fields: date, order: DESC }, limit: 5) {
