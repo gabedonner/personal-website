ff298168b74dfc74eb801dfc947e8393979859e8
gdonner <gtdonner1@gmail.com>
2022-02-21 22:50:24 -0500

    page transitionsgit push origin main

--- src/@lekoarts/gatsby-theme-minimal-blog/gatsby-browser.js
+++ src/@lekoarts/gatsby-theme-minimal-blog/gatsby-browser.js
@@ -10,18 +10,21 @@ export const wrapPageElement = ({element}) => (
 
 // scroll position magic to consider later
 
-/* export const shouldUpdateScroll = ({
+export const shouldUpdateScroll = ({
   routerProps: { location },
   getSavedScrollPosition
 }) => {
+  // transition duration from `layout.js` * 1000 to get time in ms
+  // * 2 for exit + enter animation
+  const TRANSITION_DELAY = 0.3 * 1000 * 2
+  // if it's a "normal" route
   if (location.action === "PUSH") {
-    window.setTimeout(() => window.scrollTo(0, 0), transitionDelay);
-  } else {
-    const savedPosition = getSavedScrollPosition(location);
-    window.setTimeout(
-      () => window.scrollTo(...(savedPosition || [0, 0])),
-      transitionDelay
-    );
+    window.setTimeout(() => window.scrollTo(0, 0), TRANSITION_DELAY)
   }
-  return false;
-}; */
+  // if we used the browser's forwards or back button
+  else {
+    const savedPosition = getSavedScrollPosition(location) || [0, 0]
+    window.setTimeout(() => window.scrollTo(...savedPosition), TRANSITION_DELAY)
+  }
+  return false
+}
