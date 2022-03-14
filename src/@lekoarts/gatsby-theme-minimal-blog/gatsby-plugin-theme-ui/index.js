d2f8649ad8c721ec4e8223a00f06384a730eee62
gdonner <gtdonner1@gmail.com>
2022-03-14 14:16:49 -0400

    adjust muted color

--- src/@lekoarts/gatsby-theme-minimal-blog/gatsby-plugin-theme-ui/index.js
+++ src/@lekoarts/gatsby-theme-minimal-blog/gatsby-plugin-theme-ui/index.js
@@ -28,7 +28,7 @@ const theme = merge(tailwind, {
         heading: tailwind.colors.white,
         divide: tailwind.colors.gray[8],
         //muted: tailwind.colors.gray[8],
-        muted: tailwind.colors.gray[6],
+        muted: tailwind.colors.gray[7],
       },
     },
   },
