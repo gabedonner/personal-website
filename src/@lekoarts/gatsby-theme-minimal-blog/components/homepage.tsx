7a476b61f5b74dc68d6a8daf640368c2edcf60a0
gdonner <gtdonner1@gmail.com>
2022-05-04 15:46:26 -0400

    prettier

--- src/@lekoarts/gatsby-theme-minimal-blog/components/homepage.tsx
+++ src/@lekoarts/gatsby-theme-minimal-blog/components/homepage.tsx
@@ -1,29 +1,28 @@
 /** @jsx jsx */
-import { jsx, AspectImage } from "theme-ui"
+import { jsx, AspectImage } from 'theme-ui'
 // import { Link, useStaticQuery, graphql } from "gatsby"
-import Layout from "./layout"
+import Layout from './layout'
 // import Title from "./title"
 // import Listing from "./listing"
 // import List from "./list"
-import useMinimalBlogConfig from "../hooks/use-minimal-blog-config"
-import useSiteMetadata from "../hooks/use-site-metadata"
+import useMinimalBlogConfig from '../hooks/use-minimal-blog-config'
+import useSiteMetadata from '../hooks/use-site-metadata'
 // import replaceSlashes from "../utils/replaceSlashes"
-import { visuallyHidden } from "../styles/utils"
+import { visuallyHidden } from '../styles/utils'
 // @ts-ignore
-import Hero from "../texts/hero"
+import Hero from '../texts/hero'
 // @ts-ignore
-import Websites from "../texts/home-websites"
+import Websites from '../texts/home-websites'
 // @ts-ignore
-import Projects from "../texts/home-projects"
+import Projects from '../texts/home-projects'
 // @ts-ignore
-import { StaticImage } from "gatsby-plugin-image"
+import { StaticImage } from 'gatsby-plugin-image'
 // @ts-ignore
 //import HeroSVG from '../svg/hero-background.inline.svg'
 import HeroSVG from '../svg/dot-graph.inline.svg'
 // import ThreeHero from '../components/threejs-hero'
 import HomepageTitle from './homepage-title'
 import HomewebItems from './homeweb-items'
-
 
 type PostsProps = {
   posts: {
@@ -41,44 +40,51 @@ type PostsProps = {
   [key: string]: any
 }
 
-
-
 const Homepage = ({ posts }: PostsProps) => {
   const { basePath, blogPath } = useMinimalBlogConfig()
   const { siteTitle } = useSiteMetadata()
-  
+
   return (
     <Layout>
       <h1 sx={visuallyHidden}>{siteTitle}</h1>
-      <section sx= {{
-        mt: '-1.1em',
-        position: 'static',
-        overflow: 'hidden',
-        height: '21.8em',
-        color: 'dots',
-      }}>
+      <section
+        sx={{
+          mt: '-1.1em',
+          position: 'static',
+          overflow: 'hidden',
+          height: '21.8em',
+          color: 'dots',
+        }}
+      >
         <HeroSVG />
-      </section>   
-      <section sx={{  
-        position: 'relative', 
-        height: '20em',
-        pb: ['4em', '6em'], pt: [0], mt: '-17.65em', mb: 0, 
-        p: { fontSize: [2, 3, 3], mt: 3 }, 
-        variant: `section_hero`,
-        overflow: 'hidden',
-        //boxShadow: 'rgb(0 0 0 / 4%) 0px 32px 32px 0px, rgb(0 0 0 / 4%) 0px 16px 16px 0px, rgb(0 0 0 / 4%) 0px 8px 8px 0px, rgb(0 0 0 / 4%) 0px 4px 4px 0px',
-        }}>
+      </section>
+      <section
+        sx={{
+          position: 'relative',
+          height: '20em',
+          pb: ['4em', '6em'],
+          pt: [0],
+          mt: '-17.65em',
+          mb: 0,
+          p: { fontSize: [2, 3, 3], mt: 3 },
+          variant: `section_hero`,
+          overflow: 'hidden',
+          //boxShadow: 'rgb(0 0 0 / 4%) 0px 32px 32px 0px, rgb(0 0 0 / 4%) 0px 16px 16px 0px, rgb(0 0 0 / 4%) 0px 8px 8px 0px, rgb(0 0 0 / 4%) 0px 4px 4px 0px',
+        }}
+      >
         <Hero />
-      </section>  
-      
-       
+      </section>
       <section>
-        <HomepageTitle title={"Client Websites"} />
+        <HomepageTitle title={'Client Websites'} />
+      </section>
+
+      <section>
+        <HomepageTitle title={'Client Websites'} />
         <HomewebItems />
       </section>
-      {/* <Websites /> */} 
-      <HomepageTitle title={"Design Projects"} />    
-       <Projects />
+      {/* <Websites /> */}
+      <HomepageTitle title={'Design Projects'} />
+      <Projects />
     </Layout>
   )
 }
