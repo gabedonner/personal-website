42a4190c0c41a7aae423fbfe26904d11c4506a4a
gdonner <gtdonner1@gmail.com>
2022-05-04 23:41:28 -0400

    add in grid

--- src/@lekoarts/gatsby-theme-minimal-blog/components/dev-project.tsx
+++ src/@lekoarts/gatsby-theme-minimal-blog/components/dev-project.tsx
@@ -1,11 +1,19 @@
 /** @jsx jsx */
-import {jsx, Text, Box} from 'theme-ui'
+import {jsx, Text, Box, Grid, Heading } from 'theme-ui'
+import { StaticImage } from "gatsby-plugin-image"
+ 
+
 
 const DevProject = () => {
     return (
-        <Box sx={{pb: 5}}>
-            <Text>this is where a dev project will go</Text>
-        </Box>
+        <Grid gap={[4]} columns={[1, 1, 2]} sx={{pb: 6, px: 4}}>
+            <StaticImage sx={{}}src="../images/iconiclinx-website-snapshot.png" alt="iconiclinx"  />
+            <Box sx={{position: 'relative', bottom: 0}}>
+                <Heading>NYT Critic's Picks</Heading>
+                <br />
+                <Text sx={{}}>A searchable collection of all the movies picked by New York Times critics. Built with Next.js, the NYT REST API, MUI, and Framer Motion.</Text>
+            </Box>
+        </Grid>
     )
 }
 
