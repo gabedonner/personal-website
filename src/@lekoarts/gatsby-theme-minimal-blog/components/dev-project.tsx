561c2c8de856b2d596cd42f8d2940821a6d6fe19
gdonner <gtdonner1@gmail.com>
2022-05-09 18:12:55 -0400

    add external links

--- src/@lekoarts/gatsby-theme-minimal-blog/components/dev-project.tsx
+++ src/@lekoarts/gatsby-theme-minimal-blog/components/dev-project.tsx
@@ -1,20 +1,30 @@
 /** @jsx jsx */
-import {jsx, Text, Box, Grid, Heading } from 'theme-ui'
-import { StaticImage } from "gatsby-plugin-image"
- 
-
+import { jsx, Text, Box, Grid, Heading, Link } from 'theme-ui'
+import { StaticImage } from 'gatsby-plugin-image'
 
 const DevProject = () => {
-    return (
-        <Grid gap={[4]} columns={[1, 1, 2]} sx={{pb: 6, px: 4}}>
-            <StaticImage sx={{}}src="../images/iconiclinx-website-snapshot.png" alt="iconiclinx"  />
-            <Box sx={{position: 'relative', bottom: 0}}>
-                <Heading>NYT Critic's Picks</Heading>
-                <br />
-                <Text sx={{}}>A searchable collection of all the movies picked by New York Times critics. Built with Next.js, the NYT REST API, MUI, and Framer Motion.</Text>
-            </Box>
-        </Grid>
-    )
+  return (
+    <Grid gap={[4]} columns={[1, 1, 2]} sx={{ pb: 6, px: 4 }}>
+      <StaticImage
+        sx={{}}
+        src="../images/iconiclinx-website-snapshot.png"
+        alt="iconiclinx"
+      />
+      <Box sx={{}}>
+        <Heading>NYT Critic's Picks</Heading>
+        <br />
+        <Text sx={{}}>
+          A searchable collection of all the movies picked by New York Times
+          critics. Built with Next.js, the NYT REST API, MUI, and Framer Motion.
+        </Text>
+        <Box sx={{ pt: 3 }}>
+          <Link>Visit Site</Link>
+          <br />
+          <Link>Github</Link>
+        </Box>
+      </Box>
+    </Grid>
+  )
 }
 
 export default DevProject
