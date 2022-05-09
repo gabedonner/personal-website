680720a7f82bd252f91406aca25e36ba3708834a
gdonner <gtdonner1@gmail.com>
2022-05-09 18:14:56 -0400

    styling

--- src/@lekoarts/gatsby-theme-minimal-blog/components/dev-project.tsx
+++ src/@lekoarts/gatsby-theme-minimal-blog/components/dev-project.tsx
@@ -4,13 +4,17 @@ import { StaticImage } from 'gatsby-plugin-image'
 
 const DevProject = () => {
   return (
-    <Grid gap={[4]} columns={[1, 1, 2]} sx={{ pb: 6, px: 4 }}>
+    <Grid
+      gap={[4]}
+      columns={[1, 1, 2]}
+      sx={{ pb: 6, px: 4, alignItems: 'start' }}
+    >
       <StaticImage
         sx={{}}
         src="../images/iconiclinx-website-snapshot.png"
         alt="iconiclinx"
       />
-      <Box sx={{}}>
+      <Box sx={{ pt: 2 }}>
         <Heading>NYT Critic's Picks</Heading>
         <br />
         <Text sx={{}}>
