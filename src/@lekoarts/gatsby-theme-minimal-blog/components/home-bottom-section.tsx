1d4da0fc27db84f7e967117cec5a910b168dae71
gdonner <gtdonner1@gmail.com>
2022-05-13 22:08:22 -0400

    refactor reveal animation

--- src/@lekoarts/gatsby-theme-minimal-blog/components/home-bottom-section.tsx
+++ src/@lekoarts/gatsby-theme-minimal-blog/components/home-bottom-section.tsx
@@ -6,6 +6,7 @@ import DivideTitle from './title-divide'
 import { Link as TLink } from 'gatsby'
 //https://blog.maximeheckel.com/posts/advanced-animation-patterns-with-framer-motion/
 //how to animate children on hover
+import RevealAnimationWrapper from './reveal-animation-wrapper'
 
 interface MiniDivideTitleProps {
   title: string
@@ -13,27 +14,28 @@ interface MiniDivideTitleProps {
 
 const MiniDivideTitle = ({ title }: MiniDivideTitleProps) => {
   return (
-    <Box
-      sx={{
-        pb: 15,
-        mb: 2,
-        //flexGrow: 1,
-        // flexBasis: [256, 190, 256, 256],
-        //flexBasis: ['27.5%'],
-      }}
-    >
-      <Text
+    <RevealAnimationWrapper>
+      <Box
         sx={{
-          fontSize: [1, 2, 2],
-          color: 'secondary',
-          bg: 'background',
-          pr: 4,
+          mb: 60,
+          //flexGrow: 1,
+          // flexBasis: [256, 190, 256, 256],
+          //flexBasis: ['27.5%'],
         }}
       >
-        {title}
-      </Text>
-      <Box sx={{ mr: 0, height: '1px', bg: 'divide', mt: '-.9em' }}></Box>
-    </Box>
+        <Text
+          sx={{
+            fontSize: [1, 2, 2],
+            color: 'secondary',
+            bg: 'background',
+            pr: 4,
+          }}
+        >
+          {title}
+        </Text>
+        <Box sx={{ mr: 0, height: '1px', bg: 'divide', mt: '-.9em' }}></Box>
+      </Box>
+    </RevealAnimationWrapper>
   )
 }
 
@@ -64,64 +66,51 @@ const HomeBottomSection = () => {
         }}
       >
         <MiniDivideTitle title="This Website" />
-        <Box sx={{ mt: 40 }}>
-          <Text>
-            This website was built using React, Gatsby, Theme UI, and Framer
-            Motion.
-          </Text>
-          <br />
-          <Box sx={{ pt: 30, fontSize: 18 }}>
-            <Link
-              href="https://github.com/gabedonner/personal-website"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              See the code →
-            </Link>
+        <RevealAnimationWrapper>
+          <Box sx={{}}>
+            <Text>
+              This website was built using React, Gatsby, Theme UI, and Framer
+              Motion.
+            </Text>
+            <br />
+            <Box sx={{ pt: 30, fontSize: 18 }}>
+              <Link
+                href="https://github.com/gabedonner/personal-website"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                See the code →
+              </Link>
+            </Box>
           </Box>
-        </Box>
+        </RevealAnimationWrapper>
       </Box>
       <Box
-        sx={{
-          mb: 4,
-          // mx: 'auto',
-          //border: '1px solid black',
-        }}
+        sx={
+          {
+            // mx: 'auto',
+            //border: '1px solid black',
+          }
+        }
       >
         <MiniDivideTitle title="See Also" />
-        {/* <Link href="/">
-          <Box
-            sx={{
-              p: [90, 25, 90, 25],
-              borderRight: '1px solid',
-              borderBottom: '1px solid',
-              borderLeft: '1px solid',
-              borderColor: 'divide',
-              textAlign: 'center',
-              transition: 'all .5s ease',
-              '&:hover': {
-                bg: 'divide',
-              },
-            }}
-          >
-            <Text sx={{}}>Arrow</Text>
+        <RevealAnimationWrapper>
+          <Box sx={{ fontSize: 18 }}>
+            <Link
+              target="_blank"
+              rel="noopener noreferrer"
+              href={`/Resume-2022.pdf`}
+            >
+              Resume →
+            </Link>
           </Box>
-        </Link> */}
-        <Box sx={{ pt: 30, fontSize: 18 }}>
-          <Link
-            target="_blank"
-            rel="noopener noreferrer"
-            href={`/Resume-2022.pdf`}
-          >
-            Resume →
-          </Link>
-        </Box>
-        <Box sx={{ pt: 20, fontSize: 18 }}>
-          <Link href="/about">About Page →</Link>
-        </Box>
-        <Box sx={{ pt: 20, fontSize: 18 }}>
-          <Link href="/design">Design Portfolio →</Link>
-        </Box>
+          <Box sx={{ pt: 20, fontSize: 18 }}>
+            <Link href="/about">About Page →</Link>
+          </Box>
+          <Box sx={{ pt: 20, fontSize: 18 }}>
+            <Link href="/design">Design Portfolio →</Link>
+          </Box>
+        </RevealAnimationWrapper>
       </Box>
     </Grid>
   )
