774dbce08643a2967dd7829225e50cc57398d470
gdonner <gtdonner1@gmail.com>
2022-05-13 23:01:27 -0400

    enter animation component

---
+++ src/@lekoarts/gatsby-theme-minimal-blog/components/enter-animation-wrapper.tsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import { motion } from 'framer-motion'
+
+type EnterAnimationWrapperProps = {
+  children: any
+  delayDuration: number
+}
+
+const EnterAnimationWrapper = ({
+  children,
+  delayDuration,
+}: EnterAnimationWrapperProps) => {
+  const sectionRevealAnimations = {
+    hidden: { opacity: 0, x: -15 },
+    visible: { opacity: 1, x: 0 },
+  }
+
+  const transition = {
+    duration: 1,
+    ease: [0.6, 0.01, -0.05, 0.9],
+    delay: delayDuration,
+  }
+
+  return (
+    <motion.div
+      variants={sectionRevealAnimations}
+      // ref={ref}
+      initial="hidden"
+      //animate={controls}
+      transition={transition}
+      whileInView="visible"
+      viewport={{ once: true, margin: '-90px' }}
+    >
+      {children}
+    </motion.div>
+  )
+}
+
+export default EnterAnimationWrapper
