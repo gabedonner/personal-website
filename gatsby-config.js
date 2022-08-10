824a633e238513fd968cbf677eba9c4ba54909b2
gdonner <gtdonner1@gmail.com>
2022-08-10 15:18:04 -0400

    add crimson text and garamond fonts

--- gatsby-config.js
+++ gatsby-config.js
@@ -85,6 +85,14 @@ module.exports = {
             name: `Cardo`,
             file: `https://fonts.googleapis.com/css2?family=Cardo:ital,wght@0,400;0,700;1,400&display=swap`,
           },
+          {
+            name: `Crimson Text`,
+            file: `https://fonts.googleapis.com/css2?family=Crimson+Text:ital,wght@0,400;0,600;0,700;1,400;1,600;1,700&display=swap`,
+          },
+          {
+            name: `Cormorant Garamond`,
+            file: `https://fonts.googleapis.com/css2?family=Cormorant+Garamond:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&display=swap`,
+          },
         ],
       },
     },
