62926fddd1c5f9c2c3a4c6ecc7f3cd357d9096e1
gdonner <gtdonner1@gmail.com>
2022-08-10 13:31:58 -0400

    first adjustments / remove typewriter component

--- src/@lekoarts/gatsby-theme-minimal-blog/components/hero-text.tsx
+++ src/@lekoarts/gatsby-theme-minimal-blog/components/hero-text.tsx
@@ -72,21 +72,21 @@ const TypewriterComponent = () => {
 const Hero = () => {
   return (
     <Box>
-      <Box sx={{ mb: 2, mt: ['.85em'] }}>
+      <Box sx={{ mb: 2, mt: ['36px'] }}>
         <Text
           sx={{
             ml: [0, '.5em'],
             pl: [0],
             fontSize: [5, 6, 6],
             fontWeight: `semibold`,
-            color: `text`,
+            color: `primary`,
           }}
         >
           Gabe Donner&nbsp;
         </Text>
       </Box>
 
-      <Box
+      {/* <Box
         sx={{
           bg: 'none',
           height: ['6em'],
@@ -98,6 +98,30 @@ const Hero = () => {
         }}
       >
         <TypewriterComponent />
+      </Box> */}
+      <Box
+        sx={{
+          bg: 'none',
+          height: ['6em'],
+          pt: 1,
+          pl: ['3px', '1.7em', '1.7em'],
+          pr: '12vw',
+          mt: [1, 1, 1],
+          mr: [0],
+        }}
+      >
+        <Text
+          sx={{
+            ml: [0, '.5em'],
+            pl: [0],
+            fontSize: [3, 4, 4],
+            fontWeight: `regular`,
+            color: `secondary`,
+          }}
+        >
+          I'm a front-end developer with an eye for design and a knack for
+          strategy
+        </Text>
       </Box>
     </Box>
   )
