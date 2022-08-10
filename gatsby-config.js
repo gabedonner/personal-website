9f662dd0ff49e949b8d79c5f3b33e88e56681d6d
gdonner <gtdonner1@gmail.com>
2022-08-10 14:17:14 -0400

    add cormorant and cardo

--- gatsby-config.js
+++ gatsby-config.js
@@ -77,6 +77,14 @@ module.exports = {
             name: `Inter`,
             file: `https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap`,
           },
+          {
+            name: `Cormorant`,
+            file: `https://fonts.googleapis.com/css2?family=Cormorant:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&display=swap`,
+          },
+          {
+            name: `Cardo`,
+            file: `https://fonts.googleapis.com/css2?family=Cardo:ital,wght@0,400;0,700;1,400&display=swap`,
+          },
         ],
       },
     },
