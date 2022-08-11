6c0f27e4f5851f4cdfaf10861727b2bd66b74264
gdonner <gtdonner1@gmail.com>
2022-08-10 21:52:20 -0400

    fixing breakpoints

--- src/@lekoarts/gatsby-theme-minimal-blog/components/hero-text.tsx
+++ src/@lekoarts/gatsby-theme-minimal-blog/components/hero-text.tsx
@@ -37,7 +37,7 @@ const RotateReveal = ({ children }: any) => {
     hidden: { opacity: 1, height: '100%' },
     visible: {
       opacity: 1,
-      transform: 'rotate(-1.2deg)',
+      transform: 'rotate(-1.5deg)',
       letterSpacing: '.03rem',
     },
   }
@@ -66,137 +66,144 @@ const RotateReveal = ({ children }: any) => {
 const Hero = () => {
   return (
     <SectionReveal>
-      <Box>
-        <Box
-          sx={{
-            mb: 2,
-            pb: '3px',
-            mt: ['0px'],
-            //transform: 'rotate(-.8deg)',
-            // ':hover': {
-            //   transform: 'rotate(-1deg)',
-            // },
-          }}
-        >
-          <RotateReveal>
+      <Box sx={{}}>
+        <Box sx={{ ml: ['auto', 0, 0, '6vw'], mr: ['1em', '12vw'] }}>
+          <Box
+            sx={{
+              pb: '5px',
+              mt: ['0px'],
+            }}
+          >
+            <RotateReveal>
+              <Text
+                sx={{
+                  ml: [0, '.6em'],
+                  // pl: [0],
+                  fontSize: ['2.5rem'],
+                  fontWeight: `semibold`,
+                  //color: `primary`,
+                  color: 'primary',
+                }}
+              >
+                Gabe Donner
+              </Text>
+            </RotateReveal>
+          </Box>
+
+          <Box
+            sx={{
+              bg: 'none',
+              //height: ['6em'],
+              pt: 1,
+              pl: [0, '5em', '5em'],
+              //pr: ['1em', '12vw'],
+              // mt: [1, 1, 1],
+              mr: [0],
+              //borderLeft: '1px solid black',
+            }}
+          >
             <Text
               sx={{
-                ml: [0, '.6em'],
-                // pl: [0],
-                fontSize: ['2rem'],
-                fontWeight: `semibold`,
-                //color: `primary`,
-                color: 'primary',
+                // ml: [0, '-.6em'],
+                pl: [],
+                fontSize: [5, 6, 6],
+                fontWeight: `500`,
+                color: `text`,
+                fontFamily: `Cormorant Garamond`,
+                fontStyle: 'italic',
+                lineHeight: '62px',
+                verticalAlign: '8px',
+                ml: [0, '-1.15em'],
               }}
             >
-              Gabe Donner
+              ⤷ &nbsp;
             </Text>
-          </RotateReveal>
-        </Box>
+            <Text
+              sx={{
+                // ml: [0, '-.6em'],
+                pl: [0],
+                fontSize: [5, 6, 6],
+                fontWeight: `500`,
+                color: `text`,
+                fontFamily: `Cormorant Garamond`,
+                fontStyle: 'italic',
+                lineHeight: ['50px', '62px'],
+              }}
+            >
+              A front-end developer with an eye for design and a mind for
+              strategy
+            </Text>
+          </Box>
 
-        <Box
-          sx={{
-            bg: 'none',
-            //height: ['6em'],
-            pt: 1,
-            pl: ['3px', '1.7em', '5em'],
-            pr: '12vw',
-            mt: [1, 1, 1],
-            mr: [0],
-            //borderLeft: '1px solid black',
-          }}
-        >
-          <Text
+          <Box
             sx={{
-              // ml: [0, '-.6em'],
-              pl: [],
-              fontSize: [5, 6, 6],
-              fontWeight: `500`,
-              color: `text`,
-              fontFamily: `Cormorant Garamond`,
-              fontStyle: 'italic',
-              lineHeight: '62px',
-              verticalAlign: '8px',
-              ml: '-1.25em',
-            }}
-          >
-            ⤷&nbsp;&nbsp;
-          </Text>
-          <Text
-            sx={{
-              // ml: [0, '-.6em'],
-              pl: [0],
-              fontSize: [5, 6, 6],
-              fontWeight: `500`,
-              color: `text`,
-              fontFamily: `Cormorant Garamond`,
-              fontStyle: 'italic',
-              lineHeight: '62px',
-            }}
-          >
-            A front-end developer with an eye for design and a mind for strategy
-          </Text>
-        </Box>
-
-        <Box
-          sx={{
-            mb: 2,
-            mt: '42px',
-            pl: ['3px', '1.7em', '5.2em'],
-            a: {
-              fontWeight: 'regular',
-              fontSize: [2, 3, 3],
-              color: 'secondary',
-              textDecoration: 'none',
-              pr: 1,
-              ':hover': {
-                color: `primary`,
+              mb: 2,
+              mt: '42px',
+              //bg: 'white',
+              //textAlign: 'center',
+              pl: [0, '1.7em', '5.2em', '5.2em'],
+              a: {
+                fontWeight: 'regular',
+                fontSize: [2, 3, 3],
+                color: 'secondary',
                 textDecoration: 'none',
-                pr: '1rem',
+                pr: 1,
+                ':hover': {
+                  color: `primary`,
+                  textDecoration: 'none',
+                  pr: '1rem',
+                  pl: '.2rem',
+                },
               },
-            },
-          }}
-        >
-          <Link href="/about">Who??</Link>
-          <Text
-            sx={{
-              fontWeight: 'medium',
-              fontSize: [2, 3, 3],
-              color: 'secondary',
-              verticalAlign: '2px',
-              ml: '.5em',
             }}
           >
-            →
-          </Text>
-          <Link sx={{ ml: '1em' }} href="/about">
-            See the code
-          </Link>
-          <Text
-            sx={{
-              fontWeight: 'medium',
-              fontSize: [2, 3, 3],
-              color: 'secondary',
-              verticalAlign: '2px',
-              ml: '.5em',
-            }}
-          >
-            →
-          </Text>
-          <Link sx={{ ml: '1em' }} href="/about">
-            Contact
-          </Link>
-          <Text
-            sx={{
-              fontWeight: 'medium',
-              fontSize: [2, 3, 3],
-              color: 'secondary',
-              verticalAlign: '2px',
-              ml: '.5em',
-            }}
-          >
-            →
-          </Text>
+            <Link href="/about">Who??</Link>
+            <Text
+              sx={{
+                fontWeight: 'medium',
+                fontSize: [2, 3, 3],
+                color: 'secondary',
+                //verticalAlign: '2px',
+                ml: '.5em',
+                fontFamily: 'Cormorant Garamond',
+                fontStyle: 'italic',
+              }}
+            >
+              →
+            </Text>
+            <Link sx={{ ml: '1em' }} href="/about">
+              See the code
+            </Link>
+            <Text
+              sx={{
+                fontWeight: 'medium',
+                fontSize: [2, 3, 3],
+                color: 'secondary',
+                //verticalAlign: '2px',
+                ml: '.5em',
+                fontFamily: `Cormorant Garamond`,
+                fontStyle: 'italic',
+              }}
+            >
+              →
+            </Text>
+            <Link sx={{ ml: '1em' }} href="/about">
+              Contact
+            </Link>
+            <Text
+              sx={{
+                fontWeight: 'medium',
+                fontSize: [2, 3, 3],
+                color: 'secondary',
+                //verticalAlign: '2px',
+                ml: '.5em',
+                fontFamily: `Cormorant Garamond`,
+                fontStyle: 'italic',
+              }}
+            >
+              →
+            </Text>
+          </Box>
         </Box>
       </Box>
     </SectionReveal>
