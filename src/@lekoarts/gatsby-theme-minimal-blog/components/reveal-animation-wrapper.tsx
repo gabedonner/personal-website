4f671cbb84ff6c7dd135ccfbe6685e9a9e0feced
gdonner <gtdonner1@gmail.com>
2022-08-11 01:39:10 -0400

    add delay prop

--- src/@lekoarts/gatsby-theme-minimal-blog/components/reveal-animation-wrapper.tsx
+++ src/@lekoarts/gatsby-theme-minimal-blog/components/reveal-animation-wrapper.tsx
@@ -1,7 +1,15 @@
 import React from 'react'
 import { motion } from 'framer-motion'
 
-const RevealAnimationWrapper = ({ children }: any) => {
+type EnterAnimationWrapperProps = {
+  children: any
+  delayDuration: number
+}
+
+const EnterAnimationWrapper = ({
+  children,
+  delayDuration,
+}: EnterAnimationWrapperProps) => {
   const sectionRevealAnimations = {
     hidden: { opacity: 0, y: 15 },
     visible: { opacity: 1, y: 0 },
@@ -10,6 +18,7 @@ const RevealAnimationWrapper = ({ children }: any) => {
   const transition = {
     duration: 1,
     ease: [0.6, 0.01, -0.05, 0.9],
+    delay: delayDuration,
   }
 
   return (
@@ -20,11 +29,11 @@ const RevealAnimationWrapper = ({ children }: any) => {
       //animate={controls}
       transition={transition}
       whileInView="visible"
-      viewport={{ once: true, margin: '-75px' }}
+      viewport={{ once: true, margin: '-90px' }}
     >
       {children}
     </motion.div>
   )
 }
 
-export default RevealAnimationWrapper
+export default EnterAnimationWrapper
